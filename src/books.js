3a25405db69e636beba9d6c05e6cf1c0e60ba8bc
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-04-25 17:20:30 +0200

    lab-done

--- src/books.js
+++ src/books.js
@@ -19,7 +19,7 @@
 // }
 
 // Book 3
-// title: Educated - A Memoir
+// title: C
 // pages: 352
 // author: Tara Westover
 // details: {
@@ -38,29 +38,68 @@
 
 
 // Your code here:
-const booksArray = [];
-
+const booksArray = [
+ {
+  title: "The Old Man and the Sea",
+  pages: 128,
+  author: "Ernest Hemingway",
+  details: {
+    language: "English",
+    description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+  details: {
+    language: "English",
+    description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+  }
+  },{title: "Tara Westover",
+  pages: 352,
+  author: "Tara Westover",
+  details: {
+  language: "English",
+  description: "Educated is an account of the struggle for self-invention..."
+    }
+  },{title: "The Arta of Learning",
+  pages: 288,
+  author: "Josh Waitzkin",
+  details: {
+    language: "English",
+    description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  }
+]
 
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(book) {
   // Your code here:
-
+return `${book.title} - ${book.author} - ${book.pages} pages`
 }
+
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
-
+delete booksArray[0].details.language;
+delete booksArray[1].details.language;
+delete booksArray[2].details.language;
+delete booksArray[3].details.language;
+console.log(booksArray)
 
 
 
 // Iteration 4 | Estimated Reading Time
 // Your code here:
-
-
+booksArray.forEach(book =>{
+  const minRead = (book.pages * 500) / 90;
+  book.readingTime = Math.ceil(minRead);
+  });
 
 
 // Bonus: Iteration 5 | Books Dictionary
